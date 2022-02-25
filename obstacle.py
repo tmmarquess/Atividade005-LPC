ca38c773eaa22eff3ebbd1fc598aebe4cc247990
Diogo Rezende <98904037+DiogoRezen@users.noreply.github.com>
2022-02-25 18:58:17 -0300

    Update obstacle.py

--- obstacle.py
+++ obstacle.py
@@ -8,7 +8,7 @@ class Obstacle(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load('img/asteroide 2 teste.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=(randint(40, config.screen_width - 40), -20))
-        self.speed = 8
+        self.speed = config.ini_vel
 
     def movement(self):
         self.rect.y += self.speed
